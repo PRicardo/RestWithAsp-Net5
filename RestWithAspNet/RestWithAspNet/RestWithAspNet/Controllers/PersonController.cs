50621b38a37f611b326c3651e4894dabc12d44a4
Paulo Ricardo Alves <paulo.alves@gft.com>
2021-05-07 18:59:15 -0300

    Adição da model e controller de de Person

---
+++ RestWithAspNet/RestWithAspNet/RestWithAspNet/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
